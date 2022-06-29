f583b785e39452d0480c4ac9756d36b03459ab7f
bensmailfati11 <100238927+bensmailfati11@users.noreply.github.com>
2022-06-29 19:25:40 +0100

    Constat App

--- app/src/main/java/ma/ofppt/myprojetversion1/MainActivity5.java
+++ app/src/main/java/ma/ofppt/myprojetversion1/MainActivity5.java
@@ -2,13 +2,27 @@ package ma.ofppt.myprojetversion1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity5 extends AppCompatActivity {
 
+    Button btn33;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        btn33 = findViewById(R.id.btn33);
+
+        btn33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(MainActivity5.this,MainActivity6.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
